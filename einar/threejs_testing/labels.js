a10c48444c605419235a2cdb80097bd45d3e59e0
dzenan <dumpor@gmail.com>
2014-03-21 20:53:39 +0100

    Rydding på labels og lights er done. Merga med Einars

--- einar/threejs_testing/labels.js
+++ einar/threejs_testing/labels.js
@@ -1,28 +1,81 @@
-// Adding month labels
-for (var i = 0; i < 12; i++) {
-  var text_label_geo = new THREE.TextGeometry(months[i], props_labels);
-  text_label = new THREE.Mesh(text_label_geo, material_labels);
-  scene.add(text_label);
-  text_label.rotation.x -= Math.PI * 0.2;
-  text_label.position.set(75*i-12,5,60);
-};
+
+prettyBars.labels = (function() {
+  // Variables that depend each run
+  var stateMap = {
+    scene: null, 
+    fromYear: null,
+    toYear: null
+  };
+
+  // Functions
+  var init,
+      initializeStateMap,
+      addYearLabels,
+      addYearLines,
+      addMonthLabels;
+
+  initializeStateMap = function(scene, from, to) {
+    stateMap.scene = scene;
+    stateMap.fromYear = from;
+    stateMap.toYear = to;
+  };
+
+  addYearLabels = function() {
+    // Loop through the years, and create a text ranging (from-to)
+    for (var i = 0; i < stateMap.toYear - stateMap.fromYear; i++) {
+      var year = stateMap.toYear - i;
+      var text_label_geo = new THREE.TextGeometry(year, props_labels);
+      text_label = new THREE.Mesh(text_label_geo, material_labels);
+      stateMap.scene.add(text_label);
+      text_label.rotation.x -= Math.PI * 0.2;
+      text_label.position.set(75*12, 5, -100*i+30);
+    }
+  };
+
+  addYearLines = function() {
+    // Creating a geometry and adding the lines for each year
+    for (var i = 0; i < stateMap.toYear - stateMap.fromYear; i++) {
+      var geometry = new THREE.Geometry();
+      geometry.vertices.push( new THREE.Vector3( 75*12+40, 15, -100*i+20 ) );
+      geometry.vertices.push( new THREE.Vector3( 75*12+40, 5, -100*i+50 ) );
+      geometry.vertices.push( new THREE.Vector3( 75*12-60, 5, -100*i+50 ) );
+
+      var line = new THREE.Line( geometry, material_line );
+      stateMap.scene.add( line );
+    }
+
+  };
+
+  addMonthLabels = function() {
+    // Loop through all the months and add a label for each one
+    for (var i = 0; i < 12; i++) {
+      var text_label_geo = new THREE.TextGeometry(months[i], props_labels);
+      text_label = new THREE.Mesh(text_label_geo, material_labels);
+      stateMap.scene.add(text_label);
+      text_label.rotation.x -= Math.PI * 0.2;
+      text_label.position.set(75*i-12,5,60);
+    }
+  };
+
+  var init = function(scene, from, to) {
+
+    // Set the dependant variables
+    initializeStateMap(scene, from, to);
+
+    // Set up the labels
+    addYearLabels();
+    addMonthLabels();
+    addYearLines();
+  };
+    
+    
+    
+  // Public functions
+  return {
+    init: init
+  };
+  
+
+}());
 
 
-// Adding year labels
-for (var i = 0; i < year_last - year_first; i++) {
-  var year = year_last - i;
-  var text_label_geo = new THREE.TextGeometry(year, props_labels);
-  text_label = new THREE.Mesh(text_label_geo, material_labels);
-  scene.add(text_label);
-  text_label.rotation.x -= Math.PI * 0.2;
-  text_label.position.set(75*12, 5, -100*i+30);
-
-  // Adding lines
-  var geometry = new THREE.Geometry();
-  geometry.vertices.push( new THREE.Vector3( 75*12+40, 15, -100*i+20 ) );
-  geometry.vertices.push( new THREE.Vector3( 75*12+40, 5, -100*i+50 ) );
-  geometry.vertices.push( new THREE.Vector3( 75*12-60, 5, -100*i+50 ) );
-
-  var line = new THREE.Line( geometry, material_line );
-  scene.add( line );
-}
--- einar/threejs_testing/lights.js
+++ einar/threejs_testing/lights.js
@@ -1,33 +1,79 @@
-// create a point light
-var pointLight_height = 500;
-var pointLight_camera = new THREE.PointLight(0xFFFFFF,1);
-pointLight_camera.position.set(
-                        camera.position.x,
-                        camera.position.y,
-                        camera.position.z);
-scene.add(pointLight_camera);
+prettyBars.lights = (function() {
+	// Variables that depend each run
+	var stateMap = {
+		scene: null,
+		camera: null,
+		lightsHeight: 500
+	};
 
-var pointLight_origo = new THREE.PointLight(0xFFFFFF,0.3);
-pointLight_origo.position.set( 0, pointLight_height, 0);
-scene.add(pointLight_origo);
+	// Functions
+	var init,
+		initializeStateMap,
+		addDirectionalLight,
+		addHemisphereLight,
+		addPointLightCamera,
+		addPointLightsBars;
 
-var pointLight_farleft = new THREE.PointLight(0xFFFFFF,0.3);
-pointLight_farleft.position.set( 0, pointLight_height, -10*100);
-scene.add(pointLight_farleft);
+		initializeStateMap = function(scene, camera, lightsHeight) {
+			stateMap.scene = scene;
+			stateMap.camera = camera;
+			stateMap.lightsHeight = lightsHeight;
+		};
 
-var pointLight_farright = new THREE.PointLight(0xFFFFFF,0.3);
-pointLight_farright.position.set( 12*75, pointLight_height, -10*100);
-scene.add(pointLight_farright);
+		addDirectionalLight = function() {
+			// Create a directional light
+			var dirLight = new THREE.DirectionalLight(0xffffff,.5);
+			dirLight.position.set(stateMap.camera.position.x, stateMap.camera.position.y, stateMap.camera.position.z);
+			stateMap.scene.add(dirLight);
+		};
 
-// Create a directional light
-var dirLight = new THREE.DirectionalLight(0xffffff,.5);
-dirLight.position.set(camera.position.x, camera.position.y, camera.position.z);
-scene.add(dirLight);
+		addHemisphereLight = function() {
+			var hemiLight = new THREE.HemisphereLight(0xffffff, 0x333333, 0.3);
+			stateMap.scene.add(hemiLight);
+		};
 
-// Create ambient light
-// var ambiLight = new THREE.AmbientLight(0xffffff);
-// scene.add(ambiLight);
+		addPointLightCamera = function() {
+			var pointLight_camera = new THREE.PointLight(0xFFFFFF,1);
+			pointLight_camera.position.set(stateMap.camera.position.x, stateMap.camera.position.y, stateMap.camera.position.z);
+			stateMap.scene.add(pointLight_camera);
+		};
 
-// Create hemisphere light
-var hemiLight = new THREE.HemisphereLight(0xffffff, 0x333333, 0.3);
-scene.add(hemiLight);
+		addPointLightsBars = function() {
+			// Adding the different lights that lie between the bars
+			var pointLight_origo = new THREE.PointLight(0xFFFFFF,0.3);
+			pointLight_origo.position.set( 0, stateMap.lightsHeight, 0);
+			stateMap.scene.add(pointLight_origo);
+			
+			var pointLight_farleft = new THREE.PointLight(0xFFFFFF,0.3);
+			pointLight_farleft.position.set( 0, stateMap.lightsHeight, -10*100);
+			stateMap.scene.add(pointLight_farleft);
+			
+			var pointLight_farright = new THREE.PointLight(0xFFFFFF,0.3);
+			pointLight_farright.position.set( 12*75, stateMap.lightsHeight, -10*100);
+			stateMap.scene.add(pointLight_farright);
+		};
+
+		init = function(scene, camera, lightsHeight) {
+			// Init the state variables
+			initializeStateMap(scene, camera, lightsHeight);
+
+			// Set up the different Lights
+			addDirectionalLight();
+			addHemisphereLight();
+			addPointLightCamera();
+			addPointLightsBars();
+		};
+
+
+
+	// Public Functions
+	return {
+		init: init
+	};
+
+
+
+}());
+
+
+
