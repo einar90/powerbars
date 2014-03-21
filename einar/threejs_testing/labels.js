92f1f8423a5a741c0a57ed04c39a2fc61ac9b298
dzenan <dumpor@gmail.com>
2014-03-21 22:33:36 +0100

    Props er slankere, skal fjerne mer men angriper raycast først

--- einar/threejs_testing/labels.js
+++ einar/threejs_testing/labels.js
@@ -7,9 +7,21 @@ prettyBars.labels = (function() {
     toYear: null
   };
 
+  var labelSettings = {
+    size: 10.0,
+    height: 1.5
+  }
+
+  // Materials
+  var materials = {
+    lineMaterial: null,
+    labelMaterial: null
+  };
+
   // Functions
   var init,
       initializeStateMap,
+      initializeMaterials,
       addYearLabels,
       addYearLines,
       addMonthLabels;
@@ -19,13 +31,25 @@ prettyBars.labels = (function() {
     stateMap.fromYear = from;
     stateMap.toYear = to;
   };
+  
+  initializeMaterials = function() {
+    materials.lineMaterial = new THREE.LineBasicMaterial(
+    {
+      color: 0x999999,
+    });
+
+    materials.labelMaterial = new THREE.MeshBasicMaterial(
+    {
+      color: 0xffffff,
+    });
+  };
 
   addYearLabels = function() {
     // Loop through the years, and create a text ranging (from-to)
     for (var i = 0; i < stateMap.toYear - stateMap.fromYear; i++) {
       var year = stateMap.toYear - i;
-      var text_label_geo = new THREE.TextGeometry(year, props_labels);
-      text_label = new THREE.Mesh(text_label_geo, material_labels);
+      var text_label_geo = new THREE.TextGeometry(year, labelSettings);
+      text_label = new THREE.Mesh(text_label_geo, materials.labelMaterial);
       stateMap.scene.add(text_label);
       text_label.rotation.x -= Math.PI * 0.2;
       text_label.position.set(75*12, 5, -100*i+30);
@@ -40,7 +64,7 @@ prettyBars.labels = (function() {
       geometry.vertices.push( new THREE.Vector3( 75*12+40, 5, -100*i+50 ) );
       geometry.vertices.push( new THREE.Vector3( 75*12-60, 5, -100*i+50 ) );
 
-      var line = new THREE.Line( geometry, material_line );
+      var line = new THREE.Line( geometry, materials.lineMaterial );
       stateMap.scene.add( line );
     }
 
@@ -48,9 +72,12 @@ prettyBars.labels = (function() {
 
   addMonthLabels = function() {
     // Loop through all the months and add a label for each one
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+              "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"];
+
     for (var i = 0; i < 12; i++) {
-      var text_label_geo = new THREE.TextGeometry(months[i], props_labels);
-      text_label = new THREE.Mesh(text_label_geo, material_labels);
+      var text_label_geo = new THREE.TextGeometry(months[i], labelSettings);
+      text_label = new THREE.Mesh(text_label_geo, materials.labelMaterial);
       stateMap.scene.add(text_label);
       text_label.rotation.x -= Math.PI * 0.2;
       text_label.position.set(75*i-12,5,60);
@@ -61,6 +88,9 @@ prettyBars.labels = (function() {
 
     // Set the dependant variables
     initializeStateMap(scene, from, to);
+
+    // Initialize module materials
+    initializeMaterials();
 
     // Set up the labels
     addYearLabels();
--- einar/threejs_testing/scenebox.js
+++ einar/threejs_testing/scenebox.js
@@ -4,9 +4,16 @@ prettyBars.scenebox = (function() {
 		scene: null
 	};
 
+	// Materials used in the module
+	var materials = {
+		materialFloor : null,
+		materialWall : null
+	};
+
 	// Functions
 	var init,
 		initializeStateMap,
+		initializeMaterials,
 		addPlaneFloor,
 		addPlanesWalls;
 
@@ -14,10 +21,23 @@ prettyBars.scenebox = (function() {
 		stateMap.scene = scene;
 	};
 
+	initializeMaterials = function() {
+		materials.materialFloor = new THREE.MeshPhongMaterial(
+			{
+			  color: 0x333333,
+			});
+
+		materials.materialWall = new THREE.MeshPhongMaterial(
+			{
+			  color: 0x111111,
+			  shininess: 10,
+			});
+	};
+
 	addPlaneFloor = function() {
 		// Creates the plane that acts as floor
 		plane_floor_geometry = new THREE.PlaneGeometry(4000,8000);
-		plane_floor = new THREE.Mesh(plane_floor_geometry, material_floor);
+		plane_floor = new THREE.Mesh(plane_floor_geometry, materials.materialFloor);
 		stateMap.scene.add(plane_floor);
 		plane_floor.rotation.x -= Math.PI*0.5;
 		plane_floor.position.y = 0;
@@ -26,20 +46,20 @@ prettyBars.scenebox = (function() {
 	addPlanesWalls = function() {
 		// Creating back wall plane
 		plane_backwall_geometry = new THREE.PlaneGeometry(4000,4000);
-		plane_backwall = new THREE.Mesh(plane_backwall_geometry, material_wall);
+		plane_backwall = new THREE.Mesh(plane_backwall_geometry, materials.materialWall);
 		stateMap.scene.add(plane_backwall);
 		plane_backwall.position.z = -4000;
 		
 		// Creating left wall plane
 		plane_leftwall_geometry = new THREE.PlaneGeometry(8000,4000);
-		plane_leftwall = new THREE.Mesh(plane_leftwall_geometry, material_wall);
+		plane_leftwall = new THREE.Mesh(plane_leftwall_geometry, materials.materialWall);
 		stateMap.scene.add(plane_leftwall);
 		plane_leftwall.rotation.y = Math.PI*0.5;
 		plane_leftwall.position.x = -2000;
 		
 		// Creating right wall plane
 		plane_rightwall_geometry = new THREE.PlaneGeometry(8000,4000);
-		plane_rightwall = new THREE.Mesh(plane_rightwall_geometry, material_wall);
+		plane_rightwall = new THREE.Mesh(plane_rightwall_geometry, materials.materialWall);
 		stateMap.scene.add(plane_rightwall);
 		plane_rightwall.rotation.y = -Math.PI*0.5;
 		plane_rightwall.position.x = 2000;
@@ -48,6 +68,9 @@ prettyBars.scenebox = (function() {
 	init = function(scene) {
 		// Initialize state variables
 		initializeStateMap(scene);
+
+		// Initialize the materials
+		initializeMaterials();
 
 		// Set up all the walls and floor
 		addPlaneFloor();
